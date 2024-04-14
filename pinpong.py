9fb6c975e21f810456b367aa9471c5899258eb9d
soskaneriaka <165564973+soskaneriaka@users.noreply.github.com>
2024-04-14 18:34:55 +0500

    Add files via upload

---
+++ pinpong.py
@@ -0,0 +1,36 @@
+
+from pygame import *
+
+back = (100, 155, 255)
+win_width = 600
+win_height = 500
+win = display.set_mode((win_width, win_height))
+win.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))    
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y	
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.r
+Напишите что-нибудь
